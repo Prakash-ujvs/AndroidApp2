47ad4c082c1acf9c4c44e7b6e82e79fce5c74f42
ujvsprakash98@gmail.com <ujvsprakash98@gmail.com>
2020-11-11 17:18:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/powerreceiver/CustomReceiver.java
@@ -0,0 +1,25 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        String  toastMessage = null;
+        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        switch (intentAction){
+            case Intent.ACTION_POWER_CONNECTED:
+                toastMessage = "Power connected!";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                toastMessage ="Power disconnected!";
+                break;
+        }
+    }
+}
